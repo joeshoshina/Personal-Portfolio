6f41b55bbe1bfd43b5bc9d35204a429d1a44c1b0
Joe Hoshina <jhoshina@uci.edu>
2025-04-20 13:38:16 -0700

    Cleaned up sectioning of css using scss and dividing it up within styles folder, now using main.scss and not index.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./index.css";
+import "./styles/main.scss";
+// import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
