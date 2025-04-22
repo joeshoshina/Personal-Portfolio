2e8ec5d162631f668828420122a164ac0da28dca
Joe Hoshina <jhoshina@uci.edu>
2025-04-21 20:26:53 -0700

    Finished testAPICall page, used bootstrap styling and own css to add some tweaks

--- src/components/testAPICall.jsx
+++ src/components/testAPICall.jsx
@@ -21,9 +21,32 @@ const TestAPICall = () => {
 
   return (
     <>
-      <div className="testbutton">Test API Call</div>
-      <button onClick={getImage}>Get Images</button>
-      <img src={imageUrls[0]} />
+      <div className="center-box">
+        <div className="text-center" id="projects-title">
+          Harvard Art Museum
+        </div>
+        <div className="api-container mx-auto">
+          <div className="img-container">
+            <div className="img-box">
+              <img
+                className="img-fluid object-fit-cover w-100 h-100"
+                src={imageUrls[0]}
+                alt="Art piece from Harvard Museum"
+              />
+            </div>
+            <div className="img-box">
+              <img
+                className="img-fluid object-fit-cover w-100 h-100"
+                src={imageUrls[1]}
+                alt="Art piece from Harvard Museum"
+              />
+            </div>
+          </div>
+          <button className="mt-4 mx-auto mb-2 img-button" onClick={getImage}>
+            Find More Art Pieces!
+          </button>
+        </div>
+      </div>
     </>
   );
 };
